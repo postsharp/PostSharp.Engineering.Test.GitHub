c35de15392739385f9e4669e79495cdeced07415
Jan Hlavac <jan@postsharp.net>
2022-03-29 14:42:26 +0200

    added more changes

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,18 +1,29 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
 // This project is not open source. Please see the LICENSE.md file in the repository root for details.
 
+using BuildGitHubTestProduct;
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
 
+var testFile = Pattern.Create( "test" );
+
 var product = new Product
 {
     ProductName = "PostSharp.Engineering.Test.GitHub",
-    Solutions = new[] { new DotNetSolution( "src\\PostSharp.Engineering.Test.GitHub.sln" ) },
+    Solutions = new Solution[] { new DotNetSolution( "src\\PostSharp.Engineering.Test.GitHub.sln" ) },
     PublicArtifacts = Pattern.Create( "PostSharp.Engineering.Test.GitHub.$(PackageVersion).nupkg" ),
-    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama }
+    Dependencies = new[] { Dependencies.PostSharpEngineering },
+    Configurations = Product.DefaultConfigurations
+        .WithValue( BuildConfiguration.Public, new BuildConfigurationInfo( 
+            MSBuildName: "Release",
+            PublicPublishers: new Publisher[]
+            {
+                new TestPublisher( testFile )
+            } ) )
 };
 
 var commandApp = new CommandApp();
---
+++ eng/src/TestPublisher.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
+// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+
+using PostSharp.Engineering.BuildTools.Build;
+using PostSharp.Engineering.BuildTools.Build.Model;
+using System;
+
+namespace BuildGitHubTestProduct;
+
+internal class TestPublisher : Publisher
+{
+    public TestPublisher(Pattern files) : base(files)
+    {
+    }
+
+    public override SuccessCode Execute(
+        BuildContext context,
+        PublishSettings settings,
+        string file,
+        BuildInfo buildInfo,
+        BuildConfigurationInfo configuration )
+    {
+        if ( Environment.GetEnvironmentVariable( "TEAMCITY_GIT_PATH" ) != null )
+        {
+            context.Console.WriteMessage( "We are on TeamCity" );
+        }
+
+        context.Console.WriteMessage( "Published." );
+        
+        return SuccessCode.Success;
+    }
+}
