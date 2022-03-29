8e6c9e6f608305b1f3d3b3e9cfacfd4da80cf0d4
Jan Hlavac <jan@postsharp.net>
2022-03-29 14:12:10 +0200

    Initial commit

---
+++ eng/src/Program.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
+// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+
+using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
+using PostSharp.Engineering.BuildTools.Dependencies.Model;
+using Spectre.Console.Cli;
+
+var product = new Product
+{
+    ProductName = "PostSharp.Engineering.Test.GitHub",
+    Solutions = new[] { new DotNetSolution( "src\\PostSharp.Engineering.Test.GitHub.sln" ) },
+    PublicArtifacts = Pattern.Create( "PostSharp.Engineering.Test.GitHub.$(PackageVersion).nupkg" ),
+    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama }
+};
+
+var commandApp = new CommandApp();
+
+commandApp.AddProductCommands( product );
+
+return commandApp.Run( args );
