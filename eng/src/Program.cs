87f912b7495dd48dd870ff676edb1682f74f44ed
Jan Hlavac <jan@postsharp.net>
2022-04-27 15:06:58 +0200

    ch

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -9,7 +9,7 @@ using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
 
-var testFile = Pattern.Create( "testsss" );
+var testFile = Pattern.Create( "testssssda" );
 
 var product = new Product( TestDependencies.GitHub )
 {
