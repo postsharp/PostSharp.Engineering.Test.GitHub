27c4630081eb98eaebdea4bb01901c2e00bc6cc7
Jan H <34675617+aweneer@users.noreply.github.com>
2022-10-21 15:19:29 +0200

    Update Program.cs

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -11,7 +11,7 @@ using Spectre.Console.Cli;
 
 var product = new Product( TestDependencies.GitHub )
 {
-    ProductName = "TEST",
+    ProductName = "PostSharp.Engineering.Test.GitHub",
     Solutions = new Solution[] { new DotNetSolution( "src\\PostSharp.Engineering.Test.GitHub.sln" ) },
     PublicArtifacts = Pattern.Create( "PostSharp.Engineering.Test.GitHub.$(PackageVersion).nupkg" ),
     Dependencies = new[] { Dependencies.PostSharpEngineering, TestDependencies.TestProduct },
