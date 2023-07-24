707fed82c40d5a652737473a8507fc61b92b3ef3
Antonín Procházka <antonin@postsharp.net>
2023-07-24 15:55:38 +0200

    Merge publisher added.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -4,6 +4,7 @@ using BuildGitHubTestProduct;
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 using Spectre.Console.Cli;
@@ -16,7 +17,13 @@ var product = new Product( TestDependencies.GitHub )
     Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering, TestDependencies.TestProduct },
     Configurations = Product.DefaultConfigurations
         .WithValue( BuildConfiguration.Public,
-            b => b with { PublicPublishers = new Publisher[] { new TestPublisher( Pattern.Create( "*.nupkg" ) ) } } )
+            b => b with
+            {
+                PublicPublishers = new Publisher[] {
+                    new TestPublisher( Pattern.Create( "*.nupkg" ) ),
+                    new MergePublisher()
+                }
+            } )
 };
 
 var commandApp = new CommandApp();
