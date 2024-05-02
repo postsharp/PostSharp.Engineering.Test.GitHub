c8de2ddf9c28ba0c65cacebe02575697704e9976
Antonín Procházka <antonin@postsharp.net>
2024-05-02 17:22:46 +0200

    V2

--- src/PostSharp.Engineering.Test.GitHub/Program.cs
+++ src/PostSharp.Engineering.Test.GitHub/Program.cs
@@ -1,19 +1,19 @@
 ﻿using My.Product;
-using System.Collections;
+//using System.Collections;
 
 new TargetClass().TargetMethod();
 
-var isUnattended = ProcessUtilities.IsCurrentProcessUnattended( new ConsoleLoggerFactory() );
+//var isUnattended = ProcessUtilities.IsCurrentProcessUnattended( new ConsoleLoggerFactory() );
 
-Console.WriteLine();
-Console.WriteLine( "--------------------------------------------" );
-Console.WriteLine();
-Console.WriteLine( $"Is unattended: {isUnattended}" );
-Console.WriteLine();
-Console.WriteLine( "--------------------------------------------" );
-Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine( "--------------------------------------------" );
+//Console.WriteLine();
+//Console.WriteLine( $"Is unattended: {isUnattended}" );
+//Console.WriteLine();
+//Console.WriteLine( "--------------------------------------------" );
+//Console.WriteLine();
 
-foreach ( DictionaryEntry ev in Environment.GetEnvironmentVariables())
-{
-    Console.WriteLine( $"{ev.Key}: {ev.Value}" );
-}
+//foreach ( DictionaryEntry ev in Environment.GetEnvironmentVariables())
+//{
+//    Console.WriteLine( $"{ev.Key}: {ev.Value}" );
+//}
