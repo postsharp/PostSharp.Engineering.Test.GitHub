753644df3341005f1ecbb3eb9bcbb453f2f6b010
Antonín Procházka <antonin@postsharp.net>
2024-08-29 15:45:59 +0200

    PostSharp.Engineering updated.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -7,27 +7,23 @@ using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
-using Spectre.Console.Cli;
 using TestDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.TestDependencies.V2023_1;
 
 var product = new Product( TestDependencies.GitHub )
 {
-    Solutions = new Solution[] { new DotNetSolution( "src\\PostSharp.Engineering.Test.GitHub.sln" ) },
+    Solutions = [new DotNetSolution( "src\\PostSharp.Engineering.Test.GitHub.sln" )],
     PublicArtifacts = Pattern.Create( "PostSharp.Engineering.Test.GitHub.$(PackageVersion).nupkg" ),
-    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering, TestDependencies.TestProduct },
+    Dependencies = [DevelopmentDependencies.PostSharpEngineering, TestDependencies.TestProduct],
     Configurations = Product.DefaultConfigurations
         .WithValue( BuildConfiguration.Public,
             b => b with
             {
-                PublicPublishers = new Publisher[] {
+                PublicPublishers =
+                [
                     new TestPublisher( Pattern.Create( "*.nupkg" ) ),
                     new MergePublisher()
-                }
+                ]
             } )
 };
 
-var commandApp = new CommandApp();
-
-commandApp.AddProductCommands( product );
-
-return commandApp.Run( args );
+return new EngineeringApp( product ).Run( args );
